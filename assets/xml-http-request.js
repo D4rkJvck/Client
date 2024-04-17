26ea0a31c80edd6995d35d9dc78d4f86ccc42e32
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-04-17 14:06:53 +0000

    ...

---
+++ assets/xml-http-request.js
@@ -0,0 +1,26 @@
+// 1./ Define URL
+const url = 'http://localhost:8080'
+
+// 2./ Create Request
+const request = new XMLHttpRequest()
+
+// 3./ Set Method
+request.open('GET', url)
+
+// 4./ Send Request
+request.send()
+
+// 5./ Handle Response
+request.onload = () => {
+    if (request.status == 200) {
+        const data = JSON.parse(request.responseText)
+        ///////////////////
+        console.log(data)
+        //////////////////
+    } else {
+        console.log('ERROR: ', request.statusText)
+    }
+}
+
+// 6./ Handle Error
+request.onerror = () => console.log('ERROR: Failed to Fetch Data')
---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fs := http.FileServer(http.Dir("/assets"))
+	http.Handle("/assets", http.StripPrefix("/assets", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	fmt.Println("Server Started on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
